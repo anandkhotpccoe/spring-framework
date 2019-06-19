eedd607c6fa4e0737ca704dc15ca39776c3d02af
Anand Khot <anandkhot.pccoe@gmail.com>
2019-06-19 17:51:45 +0530

    Using @PostConstruct and @PreDestroy

--- src/com/anand/spring/Circle.java
+++ src/com/anand/spring/Circle.java
@@ -1,10 +1,10 @@
 package com.anand.spring;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Required;
+
 
 public class Circle implements Shape {
 
@@ -25,6 +25,19 @@ public class Circle implements Shape {
 	@Resource()
 	public void setCenter(Point center) {
 		this.center = center;
+	}
+	
+	
+	@PostConstruct
+	public void initializeCircle() {
+		System.out.println("Init Method of Circle");
+		
+	}
+	
+	@PreDestroy
+	public void destroyCicle()
+	{
+		System.out.println("Destroy Method of Circle");
 	}
 	
 	
